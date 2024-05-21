01b211ae53f6f40561e1f521fedbc44addb5c206
Nelly-Kristine <nellykristineb@gmail.com>
2024-05-21 20:59:00 +0200

    dsf

--- code/menu.js
+++ code/menu.js
@@ -1,39 +1,35 @@
-
-/*const myList = document.getElementById('myList');
-const myButton = document.getElementById('myButton');
-
-myButton.addEventListener("click", openmenu);
-
-function openmenu() {
-    if (myList.style.display != 'block') {
-        myList.style.display = 'block';
-    } else {
-        myList.style.display = 'none';
-    }
-    console.log('clicked');
-}*/
-
 // map settings
 const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 
+// Laster kartet zoomet på 4,48 og på USA
 var map = L.map('map1').setView([39.9159986, -99.1299995], 4.48);
-/*let marker = L.marker([41.1471676564501, -73.93664063232752]).addTo(map)
-let marker2 = L.marker([40.71479516263315, -73.99504510413881]).addTo(map)
-let marker3 = L.marker([34.063573394896316, -118.37661028922777]).addTo(map)
-let marker4 = L.marker([45.51946994765605, -122.61596061366485]).addTo(map)*/
 let tileURL = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
 const tiles = L.tileLayer(tileURL, { attribution })
 
+// Funksjonen for å laste opp breweries når man bruker søkefeltet og trykket på søk
+// async betyr at denne funksjonen benytter asykrone funksjonskall
 async function show_me() {
+    // Henter stat fra søkefelt
     let place = document.getElementById("searchbar").value;
+    // Link til API funksjonen for å søke opp breweris i ønsket stat.
     api_url = 'https://api.openbrewerydb.org/breweries?by_state=' + place;
+    
+    // fetch() kjører API funksjonen. Funskjonen gir et Promise tilbake. Denne bruker vi til å vente på at 
+    // funksjonen er ferdig.
     let response = await fetch(api_url);
+
+    // Her venter vi på at funksjonen skal bli ferdig. Når den er ferdig ber vi om å få resultat i JSON
     let data = await response.json();
+
+    // Nå har vi fått svarer fra serveren i format av JSON og går igjennom alle breweries og legger på kartet
     data.forEach(element => {
-        console.log(element.latitude);
-        console.log(element.longitude);
+        //console.log(element.latitude);
+        //console.log(element.longitude);
+        // Først sjekker vi om breweriet har longitude og latitude, ellers så kræsjer det
         if (element.longitude != null && element.latitude != null) {
+            // Her lager vi en ny markør på kartet
             let marker = L.marker([element.latitude, element.longitude]).addTo(map);
+            // BindPopup funksjonen legger til en liten tekstvindu når brukeren klikker på markøren.
             marker.bindPopup(`
             <b>${element.name}</b><br>
             ${element.city}<br>
